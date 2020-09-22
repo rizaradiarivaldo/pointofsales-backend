6dfd9b02732d85d5cf31bc2e50c527a12b8a0ca8
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2020-09-22 20:42:42 +0700

    backend api

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,6 +4,7 @@ const env = require("../helpers/env");
 const connection = mysql.createConnection({
   host: env.host,
   user: env.user,
+  password: env.password,
   database: env.database,
   dateStrings: "date",
 });
